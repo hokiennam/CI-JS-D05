b930d52ee853c188e2ea4823a453e0c9f999adc5
hokiennam <71180155+hokiennam@users.noreply.github.com>
2020-11-09 10:03:35 +0700

    test

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -1,10 +1,24 @@
 import React from 'react'
+const on_img = './on.jpg';
+const off_img = './of.jpg';
 
-export default function ({quoteAuthor,quoteText}){
-    return (
-        <div style={{display:'block',margin:'auto'}} >
-            <h1>{quoteAuthor}</h1>
-            <p>{quoteText}</p>
-        </div>
-    )
+export class Bulb extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isOn: true
+        }
+        this.switch_bulb = this.switch_bulb.bind(this)
+    }
+    switch_bulb() {
+        this.setState({isOn:!this.state.isOn,new_p:100})
+    }
+    render() {
+        return (
+            <div>
+                <img src={this.state.isOn ? on_img : off_img} />
+                <input type="button" value="Switch" onClick={this.switch_bulb.bind(this)} />
+            </div>
+        )
+    }
 }
