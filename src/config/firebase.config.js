48d273d836f5137604934b71c5540104559f079b
Your Name <you@example.com>
2020-11-24 20:05:37 +0700

    to do App

---
+++ src/config/firebase.config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAreBfxA2tCsV2itgLNAMV05X9NTNORUnY",
+    authDomain: "dbp-cijs-d05.firebaseapp.com",
+    databaseURL: "https://dbp-cijs-d05.firebaseio.com",
+    projectId: "dbp-cijs-d05",
+    storageBucket: "dbp-cijs-d05.appspot.com",
+    messagingSenderId: "588746465760",
+    appId: "1:588746465760:web:62d78d2ef36d5b85e20b85",
+    measurementId: "G-YN8ZCZTXBB"
+  };
