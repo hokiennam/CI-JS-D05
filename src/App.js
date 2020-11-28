96d58d51005635c332845c7569b2a147463523ee
Your Name <you@example.com>
2020-11-28 20:47:44 +0700

    bookfinder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import React from 'react'
-import Quote from './Components/Quote'
-import { quotes } from './quotes'
+import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
+import './util.css';
+// import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import Gallery from './Components/Gallery.js';
+import { FormGroup, FormControl, InputGroup} from 'react-bootstrap'
 
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      index: 0
-    }
-    this.changeQuote = this.changeQuote.bind(this)
+      query: '',
+      items: []
+    };
   }
-  changeQuote() {
-    let index = Math.floor(Math.random() * quotes.length)
-    console.log(index)
-    this.setState({ ...this.state, index: index })
+
+  search() {
+    const API_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
+    fetch(`${API_URL}${this.state.query}`)
+      .then(response => response.json())
+      .then(json => {
+        let {items} = json;
+        this.setState({items})
+      }); // TODO: Add a catch method here in case the API call fails
   }
+
   render() {
-    return 
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Book Finder</h1>
+        </header>
+        <div className="container main-content">
+          <FormGroup>
+            <InputGroup>
+              <FormControl type="text" placeholder="Search for a book"
+              onChange={ event => this.setState({ query: event.target.value }) }
+              onKeyPress={ event => {
+                if ('Enter' === event.key) {
+                  this.search();
+                }
+              }} />
+              <InputGroup.Addon onClick={() => this.search()}>
+                <Glyphicon glyph="search"/>
+              </InputGroup.Addon>
+            </InputGroup>
+          </FormGroup>
+          <Gallery items={this.state.items} />
+        </div>
+      </div>
+    );
   }
 }
 
